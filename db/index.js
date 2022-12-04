f8b9bf4cc4f54a259d1750ed4289528b16af5557
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-12-03 22:27:36 -0500

    Fixed bugs

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 let dbUrl = process.env.MONGODB_URI
 
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 // Middleware
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Routes
 app.use('/api', apiRouter)
